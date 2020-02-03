2beab2f664bf8ff4ee5c5022f83fc6dedc4bf0a2
obie-udmz <obie.umdz@gmail.com>
2020-02-02 21:28:19 -0500

    firstcommit

---
+++ gt.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter 1st num? "))
+num2 = int(input("enter 2nd num? "))
+# Add two numbers
+sum = float(num1) + float(num2)
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
